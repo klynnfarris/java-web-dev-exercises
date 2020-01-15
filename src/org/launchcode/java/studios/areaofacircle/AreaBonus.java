464c01d7b5453602466effaed9d7184d99d31766
Katie <klynnfarris@gmail.com>
2020-01-15 04:30:05 -0600

    2.4 exercises

--- src/org/launchcode/java/studios/areaofacircle/AreaBonus.java
+++ src/org/launchcode/java/studios/areaofacircle/AreaBonus.java
@@ -2,23 +2,23 @@ package org.launchcode.java.studios.areaofacircle;
 
 import java.util.Scanner;
 
-public class Area {
+public class AreaBonus {
     public static void main(String[] args) {
-        double radius;
+        //double radius;
+        String radius;
         double area;
         double pi = 3.14;
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter the radius of a circle: ");
-        radius = input.nextDouble();
+        //radius = input.nextDouble();
+        radius = input.nextLine();
         //area = pi * (radius * radius);
-        area = Circle.getArea(radius);
-        if (radius < 0 ){
+
+        if (radius == null ){
             System.out.println("ERROR");
         } else
-        System.out.println("The area of a circle with a radius of " + radius + " is: " + area);
+            System.out.println("The area of a circle with a radius of " + radius + " is: ");
 
     }
 }
-
-//what data type will accept anything, casting etc accept is he asid reassign as a string then check to see
